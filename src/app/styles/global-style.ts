d514b4ce16a7f1a1f398ef94254aabadd1c81505
José Lucas <joselucasa937@gmail.com>
2023-12-01 19:41:24 -0300

    update

---
+++ src/app/styles/global-style.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: red;
+  }
+`;
