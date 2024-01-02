e45804904a9b4385d1e8f2f51df8b00090434ce9
José Lucas <joselucasa937@gmail.com>
2024-01-02 18:57:15 -0300

    atualizando o tipo postData

--- src/app/services/get-post.ts
+++ src/app/services/get-post.ts
@@ -3,9 +3,9 @@ import { POST_URL } from '../config/app-config';
 import { PostData } from '../posts/post';
 import { fetchJson } from './fetchJson';
 
-export const getPost = async (id: number | number[]): Promise<PostData[]> => {
+export const getPost = async (id: number | number[]): Promise<PostData> => {
   const idNumber = Array.isArray(id) ? id[0] : id;
   const url = `${POST_URL}/${idNumber}?populate=*`;
-  const posts = await fetchJson<PostData[]>(url);
+  const posts = await fetchJson<PostData>(url);
   return posts;
 };
